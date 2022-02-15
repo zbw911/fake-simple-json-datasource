9dee8a2e045568fdcaf0b2204260dcd10d8de131
zhangbaowei <zbw911@gmail.com>
2022-02-15 11:01:49 +0800

    TO SINGLE URL

--- index.js
+++ index.js
@@ -10,149 +10,224 @@ var countryTimeseries = require('./country-series');
 
 var now = Date.now();
 
-for (var i = timeserie.length -1; i >= 0; i--) {
-  var series = timeserie[i];
-  var decreaser = 0;
-  for (var y = series.datapoints.length -1; y >= 0; y--) {
-    series.datapoints[y][1] = Math.round((now - decreaser) /1000) * 1000;
-    decreaser += 50000;
-  }
+for (var i = timeserie.length - 1; i >= 0; i--) {
+    var series = timeserie[i];
+    var decreaser = 0;
+    for (var y = series.datapoints.length - 1; y >= 0; y--) {
+        series.datapoints[y][1] = Math.round((now - decreaser) / 1000) * 1000;
+        decreaser += 50000;
+    }
 }
 
 var annotation = {
-  name : "annotation name",
-  enabled: true,
-  datasource: "generic datasource",
-  showLine: true,
+    name: "annotation name",
+    enabled: true,
+    datasource: "generic datasource",
+    showLine: true,
 }
 
 var annotations = [
-  { annotation: annotation, "title": "Donlad trump is kinda funny", "time": 1450754160000, text: "teeext", tags: "taaags" },
-  { annotation: annotation, "title": "Wow he really won", "time": 1450754160000, text: "teeext", tags: "taaags" },
-  { annotation: annotation, "title": "When is the next ", "time": 1450754160000, text: "teeext", tags: "taaags" }
+    {
+        annotation: annotation,
+        "title": "Donlad trump is kinda funny",
+        "time": 1450754160000,
+        text: "teeext",
+        tags: "taaags"
+    },
+    {annotation: annotation, "title": "Wow he really won", "time": 1450754160000, text: "teeext", tags: "taaags"},
+    {annotation: annotation, "title": "When is the next ", "time": 1450754160000, text: "teeext", tags: "taaags"}
 ];
 
 var tagKeys = [
-  {"type":"string","text":"Country"}
+    {"type": "string", "text": "Country"}
 ];
 
 var countryTagValues = [
-  {'text': 'SE'},
-  {'text': 'DE'},
-  {'text': 'US'}
+    {'text': 'SE'},
+    {'text': 'DE'},
+    {'text': 'US'}
 ];
 
 var now = Date.now();
 var decreaser = 0;
-for (var i = 0;i < annotations.length; i++) {
-  var anon = annotations[i];
+for (var i = 0; i < annotations.length; i++) {
+    var anon = annotations[i];
 
-  anon.time = (now - decreaser);
-  decreaser += 1000000
+    anon.time = (now - decreaser);
+    decreaser += 1000000
 }
 
 var table =
-  {
-    columns: [{text: 'Time', type: 'time'}, {text: 'Country', type: 'string'}, {text: 'Number', type: 'number'}],
-    rows: [
-      [ 1234567, 'SE', 123 ],
-      [ 1234567, 'DE', 231 ],
-      [ 1234567, 'US', 321 ],
-    ]
-  };
-  
+    {
+        columns: [{text: 'Time', type: 'time'}, {text: 'Country', type: 'string'}, {text: 'Number', type: 'number'}],
+        rows: [
+            [1234567, 'SE', 123],
+            [1234567, 'DE', 231],
+            [1234567, 'US', 321],
+        ]
+    };
+
 function setCORSHeaders(res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  res.setHeader("Access-Control-Allow-Headers", "accept, content-type");  
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST");
+    res.setHeader("Access-Control-Allow-Headers", "accept, content-type");
 }
 
 
 var now = Date.now();
 var decreaser = 0;
-for (var i = 0;i < table.rows.length; i++) {
-  var anon = table.rows[i];
+for (var i = 0; i < table.rows.length; i++) {
+    var anon = table.rows[i];
 
-  anon[0] = (now - decreaser);
-  decreaser += 1000000
+    anon[0] = (now - decreaser);
+    decreaser += 1000000
 }
 
-app.all('/', function(req, res) {
-  setCORSHeaders(res);
-  res.send('I have a quest for you!');
-  res.end();
-});
+app.all('/a', function (req, res) {
 
-app.all('/search', function(req, res){
-  setCORSHeaders(res);
-  var result = [];
-  _.each(timeserie, function(ts) {
-    result.push(ts.target);
-  });
+    setCORSHeaders(res);
+    console.log(req.url, req.params, req.query, req.query['cmd'], req.body)
+    var  cmd = req.query['cmd'];
+    switch (cmd) {
+        case 'search': {
+            var result = [];
+            _.each(timeserie, function (ts) {
+                result.push(ts.target);
+            });
 
-  res.json(result);
-  res.end();
-});
+            res.json(result);
+            break;
+        }
+        case 'annotations':{
+            res.json(annotations);
+            break;
+        }
+        case 'query':{
+            var tsResult = [];
+            let fakeData = timeserie;
 
-app.all('/annotations', function(req, res) {
-  setCORSHeaders(res);
-  console.log(req.url);
-  console.log(req.body);
+            if (req.body.adhocFilters && req.body.adhocFilters.length > 0) {
+                fakeData = countryTimeseries;
+            }
 
-  res.json(annotations);
-  res.end();
-});
+            _.each(req.body.targets, function (target) {
+                if (target.type === 'table') {
+                    tsResult.push(table);
+                } else {
+                    var k = _.filter(fakeData, function (t) {
+                        return t.target === target.target;
+                    });
 
-app.all('/query', function(req, res){
-  setCORSHeaders(res);
-  console.log(req.url);
-  console.log(req.body);
+                    _.each(k, function (kk) {
+                        tsResult.push(kk)
+                    });
+                }
+            });
 
-  var tsResult = [];
-  let fakeData = timeserie;
+            console.log(tsResult);
 
-  if (req.body.adhocFilters && req.body.adhocFilters.length > 0) {
-    fakeData = countryTimeseries;
-  }
-
-  _.each(req.body.targets, function(target) {
-    if (target.type === 'table') {
-      tsResult.push(table);
-    } else {
-      var k = _.filter(fakeData, function(t) {
-        return t.target === target.target;
-      });
-
-      _.each(k, function(kk) {
-        tsResult.push(kk)
-      });
+            res.json(tsResult);
+            break;
+        }
+        case 'tag-keys':{
+            res.json(tagKeys);
+            break;
+        }
+        case 'tag-values':{
+            if (req.body.key == 'City') {
+                res.json(cityTagValues);
+            } else if (req.body.key == 'Country') {
+                res.json(countryTagValues);
+            }
+            break;
+        }
+        default: {
+            res.send('I have a quest for you!');
+            break;
+        }
     }
-  });
- 
-  res.json(tsResult);
-  res.end();
+
+    res.end();
 });
 
-app.all('/tag[\-]keys', function(req, res) {
-  setCORSHeaders(res);
-  console.log(req.url);
-  console.log(req.body);
+// app.all('/a/', function (req, res) {
+//
+//     setCORSHeaders(res);
+//     res.send('I have a quest for you!');
+//     console.log('I have a quest for you!')
+//     res.end();
+// });
 
-  res.json(tagKeys);
-  res.end();
-});
+// app.all('/a/search', function (req, res) {
+//     setCORSHeaders(res);
+//     var result = [];
+//     _.each(timeserie, function (ts) {
+//         result.push(ts.target);
+//     });
+//
+//     res.json(result);
+//     res.end();
+// });
 
-app.all('/tag[\-]values', function(req, res) {
-  setCORSHeaders(res);
-  console.log(req.url);
-  console.log(req.body);
+// app.all('/a/annotations', function (req, res) {
+//     setCORSHeaders(res);
+//     console.log(req.url);
+//     console.log(req.body);
+//
+//     res.json(annotations);
+//     res.end();
+// });
 
-  if (req.body.key == 'City') {
-    res.json(cityTagValues);
-  } else if (req.body.key == 'Country') {
-    res.json(countryTagValues);
-  }
-  res.end();
+// app.all('/a/query', function (req, res) {
+//     setCORSHeaders(res);
+//     console.log(req.url);
+//     console.log(req.body);
+//
+//     var tsResult = [];
+//     let fakeData = timeserie;
+//
+//     if (req.body.adhocFilters && req.body.adhocFilters.length > 0) {
+//         fakeData = countryTimeseries;
+//     }
+//
+//     _.each(req.body.targets, function (target) {
+//         if (target.type === 'table') {
+//             tsResult.push(table);
+//         } else {
+//             var k = _.filter(fakeData, function (t) {
+//                 return t.target === target.target;
+//             });
+//
+//             _.each(k, function (kk) {
+//                 tsResult.push(kk)
+//             });
+//         }
+//     });
+//
+//     res.json(tsResult);
+//     res.end();
+// });
+
+// app.all('/a/tag[\-]keys', function (req, res) {
+//     setCORSHeaders(res);
+//     console.log(req.url);
+//     console.log(req.body);
+//
+//     res.json(tagKeys);
+//     res.end();
+// });
+
+app.all('/a/tag[\-]values', function (req, res) {
+    setCORSHeaders(res);
+    console.log(req.url);
+    console.log(req.body);
+
+    if (req.body.key == 'City') {
+        res.json(cityTagValues);
+    } else if (req.body.key == 'Country') {
+        res.json(countryTagValues);
+    }
+    res.end();
 });
 
 app.listen(3333);
